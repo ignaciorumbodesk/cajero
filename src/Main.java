4f2c4df19f73377b66c52b3b9aeb7c65cf2ce81d
Ignacio NoteBook <nacho.r.85@hotmail.com>
2023-02-12 21:23:22 -0300

    secction 1 y 2 fully operative.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
  /*
    * DESARROLLAR UN HOME BANKING CON LAS SIGUIENTES OPERACIONES
-   * Logueo con nombre de usuario y contraseña
-   * saldo de cuenta
-   * opcion de ingreso y extraccion(preguntar si se desea seguir retirando) de dinero
-   * opcion de transferencia por CBU, ALIAS
-   * cierre de sesion
+   * 1- Logueo con nombre de usuario y contraseña simple (luego se hara por conexion a MySQL DB)
+   * 2- saldo de cuenta
+   * 3- opcion de ingreso y extraccion(preguntar si se desea seguir retirando) de dinero
+   * 4- opcion de transferencia por CBU, ALIAS
+   * 5- cierre de sesion
    **/
-
 
  import java.util.Scanner;
 
@@ -41,15 +40,15 @@
 
 
     //Metodos
-    static void messages(String userMessage) {
+    public static void messages(String userMessage) {
         System.out.println(userMessage);
     }
 
-    static void sessionFinish() {
+    public static void sessionFinish() {
         System.out.println("La Sesion ha finalizado correctamente.");
     }
 
-    static void userAuthentication(String userInputName, String userInputPassword) {
+    private static void userAuthentication(String userInputName, String userInputPassword) {
          String user = "ignacio"; //user name from database
          String pass = "1234"; //user password from database
 
@@ -58,17 +57,21 @@
         } else {
             System.out.println("Verifique los datos ingresados");
         }
-    
+
     }
+
+
 
     public static void operationMenu(){
         short userOptionMenu = 0;
         float cashOut, multipleNumber = 10, rest;
         float balance = 5000;
-        String goBack;
+        String goBack = "s";
 
         Scanner keyboardInputFunc;
         keyboardInputFunc = new Scanner(System.in);
+        //goBack = new Scanner(System.in);
+
         do {
             System.out.println("(1)-Saldos.");
             System.out.println("(2)-Exctracciones");
@@ -91,22 +94,22 @@
                 case 2:
                     messages("+++++ Extracciones +++++");
 
+                    do {
+                        System.out.println("Ingrese la cantidad que desea retirar");
+                        cashOut = keyboardInputFunc.nextFloat();
+                        rest = cashOut % multipleNumber;
 
-                    System.out.println("Ingrese la cantidad que desea retirar");
-
-                    cashOut = keyboardInputFunc.nextFloat();
-
-                    rest = cashOut % multipleNumber;
-                    if (rest == 0) {
-                        balance -= cashOut;
-                        System.out.println("La extraccion se realizo correctamente.");
-                        System.out.println("Su saldo de cuenta actual es: $" + balance + ".");
-
-
-                    } else {
-                        System.out.println("El monto debe ser multiplo de 10.");
-                    }
-
+                        if (rest == 0) {
+                            balance -= cashOut;
+                            System.out.println("La extraccion se realizo correctamente.");
+                            System.out.println("Su saldo de cuenta actual es: $" + balance + ".");
+                            System.out.println("¿Desea realizar otro retiro?.");
+                            System.out.println("presione -S- para confirmar o -N- para volver al menu principal.");
+                            goBack = keyboardInputFunc.next();
+                        } else {
+                            System.out.println("El monto debe ser multiplo de 10.");
+                        }
+                    } while (goBack.equals("s"));
                     break;
                 case 3:
                     messages("+++++ DEPOSITO EN CUENTA PROPIA +++++");
